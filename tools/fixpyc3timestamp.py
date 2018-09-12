336791a7c209a47927782d90108844a12ba8be2c
Jiří Janoušek <janousek.jiri@gmail.com>
2018-09-12 14:11:43 +0200

    Don't attempt to fix read-only Python stuff
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- tools/fixpyc3timestamp.py
+++ tools/fixpyc3timestamp.py
@@ -11,24 +11,31 @@ OSTREE_TIMESTAMP = bytes(4)
 for root, dirs, files in os.walk(sys.argv[1]):
     for filename in files:
         path = os.path.join(root, filename)
-        if filename.endswith(('.opt-1.pyc', '.opt-2.pyc')):
-            with open(path, 'r+b') as f:
-                magic = f.read(4)
-                timestamp = f.read(4)
-                if timestamp != OSTREE_TIMESTAMP:
-                    #~ print("%s %s %s" % (filename, binascii.hexlify(magic), binascii.hexlify(timestamp)))
-                    f.seek(0)
+        try:
+            if filename.endswith(('.opt-1.pyc', '.opt-2.pyc')):
+                with open(path, 'r+b') as f:
                     magic = f.read(4)
-                    f.write(OSTREE_TIMESTAMP)
-                    f.seek(4)
                     timestamp = f.read(4)
-                    #~ print("%s %s %s" % (filename, binascii.hexlify(magic), binascii.hexlify(timestamp)))
-                    print("Fixed timestamp:", path)
-        elif filename.endswith('.egg'):
-            path = os.path.join(root, filename)
-            with open(path, "rb") as f:
-                data = f.read()
-            data = TIMESTAMP_RE.sub(b'70-Jan-01 00:00', data)
-            with open(path, "wb") as f:
-                f.write(data)
-            print("Fixed timestamp:", path)
+                    if timestamp != OSTREE_TIMESTAMP:
+                        #~ print("%s %s %s" % (filename, binascii.hexlify(magic), binascii.hexlify(timestamp)))
+                        f.seek(0)
+                        magic = f.read(4)
+                        f.write(OSTREE_TIMESTAMP)
+                        f.seek(4)
+                        timestamp = f.read(4)
+                        #~ print("%s %s %s" % (filename, binascii.hexlify(magic), binascii.hexlify(timestamp)))
+                        print("Fixed timestamp:", path)
+            elif filename.endswith('.egg'):
+                path = os.path.join(root, filename)
+                with open(path, "rb") as f:
+                    data = f.read()
+                data = TIMESTAMP_RE.sub(b'70-Jan-01 00:00', data)
+                with open(path, "wb") as f:
+                    f.write(data)
+                print("Fixed timestamp:", path)
+        except OSError as e:
+            if e.errno == 30:
+                print('Cannot fix, read-only:', path)
+            else:
+                raise
+    
