81e99628b483da9ab654fee97d2034b479b980ab
Jiří Janoušek <janousek.jiri@gmail.com>
2021-11-21 13:31:01 +0100

    Update propagate script with more files

--- propagate.sh
+++ propagate.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-for manifest in eu.tiliado.NuvolaCdk.yml eu.tiliado.NuvolaAdk.yml; do
+for manifest in eu.tiliado.NuvolaCdk.yml eu.tiliado.NuvolaAdk.yml eu.tiliado.NuvolaBase.yml eu.tiliado.NuvolaApp.yml; do
   source="experimental"
 
   for target in master stable; do
-    set +e
-    git diff manifests/$source/$manifest | patch -N manifests/$target/$manifest
-    test $? -lt 2 || exit 1
-    set -e
+    if [ -f manifests/$source/$manifest ]; then
+      set +e
+      git diff manifests/$source/$manifest | patch -N manifests/$target/$manifest
+      test $? -lt 2 || exit 1
+      set -e
+    else
+      echo skip manifests/$source/$manifest - not found
+    fi
     source=$target
   done
 done
